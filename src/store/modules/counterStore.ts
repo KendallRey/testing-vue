1820e61c9ebfa519bb686668910a2342da7dda6b
KendallRey <107897863+KendallRey@users.noreply.github.com>
2024-12-10 09:56:47 +0800

    update sample store function

--- src/store/modules/counterStore.ts
+++ src/store/modules/counterStore.ts
@@ -9,6 +9,9 @@ export const useCounterStore = defineStore('counter', {
     increment() {
       this.count++
     },
+    decrement() {
+      this.count--
+    },
   },
 })
 
--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/store/modules/counterStore'
+
+const store = useCounterStore()
+</script>
+
 <template>
   <div>
     <h1>Counter Page</h1>
+    <p>count is {{ store.count }}</p>
+    <p>Double count is {{ store.doubleCount }}</p>
+    <button @click="store.increment">increment</button>
+    <button @click="store.decrement">decrement</button>
   </div>
 </template>
